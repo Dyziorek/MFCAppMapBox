f543a01a2e24ac7406dd3e263313627b83aa2627
ddus <ddusinfo@gmail.com>
2015-11-05 16:55:29 +0100

    Checkd

--- MFCAppMapBox/MFCAppMapBoxDlg.cpp
+++ MFCAppMapBox/MFCAppMapBoxDlg.cpp
@@ -108,6 +108,22 @@ BOOL CMFCAppMapBoxDlg::OnInitDialog()
 
 	// TODO: Add extra initialization here
 	
+	//THREADNAME_INFO info;
+	//info.dwType = 0x1000;
+	//info.szName = name.c_str();
+	//info.dwThreadID = GetThreadId(static_cast<HANDLE>(thread.native_handle()));
+	//info.dwFlags = 0;
+
+	//__try
+	//{
+	//	RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+	//}
+	//__except (EXCEPTION_EXECUTE_HANDLER)
+	//{
+	//}
+
+
+
 	viewer = std::make_unique<GLFWView>();
 	
 	mbgl::DefaultFileSource fsrc(new mbgl::SQLiteCache("cache-db"));
